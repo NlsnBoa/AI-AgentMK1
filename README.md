d536c133528e9bf7db0a8baed83a6591057c61ae
Nelson Herrera <ndanielherrera@icloud.com>
2024-03-01 16:53:59 -0500

    "Fixed some things"

---
+++ README.md
@@ -0,0 +1,15 @@
+# Python and Javascript Image to Text with Chat-GPT Vision
+
+- Ensure Python3 is available
+- create an ENV with your openai api key
+- download the openai vm. This will ensure evey package you might need is ready
+
+```markdown
+python3 -m venv openai-env
+```
+
+- Then activate the enviroment using:
+
+```markdown
+source openai-env/bin/activate
+```
--- screenshot.js
+++ screenshot.js
@@ -2,39 +2,47 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
-const url = process.argv[2];
-const timeout = 5000;
+// const url = process.argv[2];
+const url = "https://www.formula1.com/en/results.html/2023/drivers.html"
+const timeout = 8000;
 
 (async () => {
-    const browser = await puppeteer.launch( {
-      headless: false,
-      executablePath: '/Applications/Google Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary',
-      userDataDir: '/Users/danyherrera/Library/Application\ Support/Google/Chrome\ Canary/Default', 
-      dumpio: true,
-    } );
+    try
+    {
+        const browser = await puppeteer.launch( {
+        headless: false,
+        executablePath: '/Applications/Google Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary',
+        userDataDir: '/Users/danyherrera/Library/Application\ Support/Google/Chrome\ Canary/Default', 
+        dumpio: true,
+        } );
 
-    const page = await browser.newPage();
+        const page = await browser.newPage();
 
-    await page.setViewport( {
-        width: 1200,
-        height: 1200,
-        deviceScaleFactor: 1,
-    } );
+        await page.setViewport( {
+            width: 1200,
+            height: 1200,
+            deviceScaleFactor: 1,
+        } );
 
-    // This navigates to the URL and waits until the page is fully loaded
-    await page.goto(url, { waitUntil: 'networkidle0', timeout: timeout });
+        // This navigates to the URL and waits until the page is fully loaded
+        await page.goto(url, { waitUntil: 'networkidle0', timeout: timeout });
 
-    // Click the Accept button
-    const frame = await page.frames().find(f => f.url().includes('https://consent.formula1.com/index.html?hasCsp=true&message_id=1033523&consentUUID=00bbffa9-53aa-4ab8-9262-5a4bf7305800&preload_message=true&version=v1'));
-    await frame.waitForSelector('[aria-label="ACCEPT ALL"]', {visible: true});
-    await frame.click('[aria-label="ACCEPT ALL"]');
+        // Click the Accept button
+        const frame = await page.frames().find(f => f.url().includes('https://consent.formula1.com/index.html?hasCsp=true&message_id=1033523&consentUUID=00bbffa9-53aa-4ab8-9262-5a4bf7305800&preload_message=true&version=v1'));
+        if (!frame == undefined) {
+            await frame.waitForSelector('[aria-label="ACCEPT ALL"]', {visible: true});
+            await frame.click('[aria-label="ACCEPT ALL"]');
+            await page.waitForTimeout(timeout);
+        }
 
-    await page.waitForTimeout(timeout);
+        await page.screenshot({
+            path: "screenshot.jpg",
+            fullPage: true,
+        });
 
-    await page.screenshot({
-        path: "screenshot.jpg",
-        fullPage: true,
-    });
-
-    await browser.close();
+        await browser.close();
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
 })();
