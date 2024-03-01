4dce7de8e5fd7980d0ca3aaf6e17152eb2e3770c
Nelson Herrera <ndanielherrera@icloud.com>
2024-03-01 14:55:03 -0500

    first commit

---
+++ screenshot.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+const url = process.argv[2];
+const timeout = 5000;
+
+(async () => {
+    const browser = await puppeteer.launch( {
+      headless: false,
+      executablePath: '/Applications/Google Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary',
+      userDataDir: '/Users/danyherrera/Library/Application\ Support/Google/Chrome\ Canary/Default', 
+      dumpio: true,
+    } );
+
+    const page = await browser.newPage();
+
+    await page.setViewport( {
+        width: 1200,
+        height: 1200,
+        deviceScaleFactor: 1,
+    } );
+
+    // This navigates to the URL and waits until the page is fully loaded
+    await page.goto(url, { waitUntil: 'networkidle0', timeout: timeout });
+
+    // Click the Accept button
+    const frame = await page.frames().find(f => f.url().includes('https://consent.formula1.com/index.html?hasCsp=true&message_id=1033523&consentUUID=00bbffa9-53aa-4ab8-9262-5a4bf7305800&preload_message=true&version=v1'));
+    await frame.waitForSelector('[aria-label="ACCEPT ALL"]', {visible: true});
+    await frame.click('[aria-label="ACCEPT ALL"]');
+
+    await page.waitForTimeout(timeout);
+
+    await page.screenshot({
+        path: "screenshot.jpg",
+        fullPage: true,
+    });
+
+    await browser.close();
+})();
